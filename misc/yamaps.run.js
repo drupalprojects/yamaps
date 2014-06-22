39139192ac6d9f35cc5b206c6625f5092027f88e
Rostislav Sergeenkov <rostislav.sergeenkov@gmail.com>
2014-06-22 23:03:32 +0300

    fixed error when adding 2 multiple fields with dynamic and static formatters

--- misc/yamaps.run.js
+++ misc/yamaps.run.js
@@ -5,10 +5,38 @@
 
 (function ($) {
   ymaps.ready(function () {
+    function creating_map(mapId, options) {
+      $('#' + mapId).once('yamaps', function () {
+        // If zoom and center are not set - set it from user's location.
+
+        if (!options.init.center || !options.init.zoom) {
+          var location = ymaps.geolocation;
+          // Set map center.
+          if (!options.init.center) {
+            // Set location, defined by ip, if they not defined.
+            options.init.center = [location.latitude, location.longitude];
+          }
+          if (!options.init.zoom) {
+            options.init.zoom = location.zoom ? location.zoom : 10;
+          }
+        }
+        // Create new map.
+        var map = new $.yaMaps.YamapsMap(mapId, options);
+        if (options.controls) {
+          // Enable controls.
+          map.enableControls();
+        }
+        if (options.traffic) {
+          // Enable traffic.
+          map.enableTraffic();
+        }
+        // Enable plugins.
+        map.enableTools();
+      });
+    }
+
     var processMaps = function () {
       if (Drupal.settings.yamaps) {
-
-        //activeMaps = [];
         for (var mapId in Drupal.settings.yamaps) {
           var options = Drupal.settings.yamaps[mapId];
           if (options.display_options.display_type === 'map_button') {
@@ -16,7 +44,6 @@
             $('#' + options.display_options.remove_button_id).hide();
             $('#' + options.display_options.open_button_id).bind({
               click: function () {
-
                 mapId = $(this).attr('mapId');
                 options = Drupal.settings.yamaps[mapId];
                 creating_map(mapId, options);
@@ -33,44 +60,32 @@
       }
     };
 
+    function processMapsStatic() {
+      if (Drupal.settings.yamapsStatic) {
+        for (var mapId in Drupal.settings.yamapsStatic) {
+          var options = Drupal.settings.yamapsStatic[mapId];
+          if (options.display_options.display_type === 'map_button') {
+            $('#' + mapId).hide();
+            $('#' + options.display_options.open_button_id).bind({
+              click: function () {
+                mapId = $(this).attr('mapId');
+                options = Drupal.settings.yamapsStatic[mapId];
+                $('#' + options.display_options.open_button_id).hide();
+                $('#' + mapId).show();
+              }
+            });
+          }
+        }
+      }
+    }
+
     // Initialize layouts.
     $.yaMaps.initLayouts();
     processMaps();
+    processMapsStatic();
 
     Drupal.behaviors.yamapsInitBehaviors = {
       attach: processMaps
     };
   });
-
-  function creating_map(mapId, options) {
-    $('#' + mapId).once('yamaps', function () {
-      // If zoom and center are not set - set it from user's location.
-
-      if (!options.init.center || !options.init.zoom) {
-        var location = ymaps.geolocation;
-        // Set map center.
-        if (!options.init.center) {
-          // Set location, defined by ip, if they not defined.
-          options.init.center = [location.latitude, location.longitude];
-        }
-        if (!options.init.zoom) {
-          options.init.zoom = location.zoom ? location.zoom : 10;
-        }
-      }
-      // Create new map.
-      var map = new $.yaMaps.YamapsMap(mapId, options);
-      if (options.controls) {
-        // Enable controls.
-        map.enableControls();
-      }
-      if (options.traffic) {
-        // Enable traffic.
-        map.enableTraffic();
-      }
-      // Enable plugins.
-      map.enableTools();
-
-      //activeMaps[mapId] = map;
-    });
-  }
 })(jQuery);
